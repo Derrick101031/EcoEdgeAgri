af8467d2825046213c31ffdbc351a67c22774bd2
Derrick101031 <43647743+Derrick101031@users.noreply.github.com>
2025-05-19 21:02:16 +0200

    Update augment.py

--- ml/ml_utils/augment.py
+++ ml/ml_utils/augment.py
@@ -1,1 +1,10 @@
+import numpy as np
+
+def augment(X, noise_std=0.01, dropout_rate=0.05):
+    Xn = X + np.random.normal(0, noise_std, X.shape)
+    Xn = np.clip(Xn, 0.0, 1.0)
+    if dropout_rate:
+        mask = np.random.rand(*Xn.shape) < dropout_rate
+        Xn[mask] = 0.0
+    return Xn
 
