11a877cecf51c48efd10ec5323552b36888dbe74
Derrick101031 <43647743+Derrick101031@users.noreply.github.com>
2025-05-19 21:00:44 +0200

    Update preprocess.py

--- ml/ml_utils/preprocess.py
+++ ml/ml_utils/preprocess.py
@@ -1,1 +1,16 @@
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+
+def fit_scaler(df_sub: pd.DataFrame) -> MinMaxScaler:
+    """Create a 0-1 MinMaxScaler for a single plant."""
+    sc = MinMaxScaler()
+    sc.fit(df_sub)
+    return sc
+
+
+def apply_scaler(df_sub: pd.DataFrame, sc: MinMaxScaler) -> pd.DataFrame:
+    """Return a copy of df_sub but scaled."""
+    df_scaled = df_sub.copy()
+    df_scaled[:] = sc.transform(df_sub)
+    return df_scaled
 
